c3e6bf09c8c49e831fa21c46d575d70cd7750d5b
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2020-04-08 18:07:13 -0700

    Solved possible bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,26 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes.empty?
+
+  i = 0 
+  while dislikes[i].empty?
+    i += 1
+  end
+
+  stack = []
+  visited = []
+  stack.push(i)
+
+  until stack.empty?
+    dog = stack.pop
+    visited.push(dog)
+
+    dislikes[dog].each do |other_dog|
+      unless visited.include?(other_dog)
+        stack.push(other_dog)
+      end
+    end
+  end
+
+  return visited.length == (dislikes.length - i)
 end
